26636e174fc04d4e43c2d4e24bfda5460be1694c
Francisco <francisco@medlmobile.com>
2020-03-09 12:26:54 -0700

    Added a hella long list of words to use

--- hangman.py
+++ hangman.py
@@ -38,3 +38,12 @@ HANGMAN_PICS = ['''
 	       /|\  |
 	       / \  |
 		=====''']
+
+words = '''ant baboon badger bad bear beaver camel cat clam cobra cougar coyote ceqo deer dog donkey duck eagle ferret
+	   fox frog goat goose hawk lion lozard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon
+	   python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'''.split()
+
+def getRandomWord(wordList):
+	#This function returns a random string from the passed list of strings above it
+	wordIndex = random.randint(0, len(wordList)-1)
+	return wordList[wordIndex]
