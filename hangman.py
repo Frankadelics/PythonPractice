fcd4c8611727119251b5844f5abf201d8fc113ff
Francisco <francisco@medlmobile.com>
2020-03-09 12:09:22 -0700

    Just the ASCII art was added

---
+++ hangman.py
@@ -0,0 +1,40 @@
+#This game is Hangman
+#Feels like this one is going to be some great typing practice
+
+import random
+HANGMAN_PICS = ['''
+		+---+
+		    |
+		    |
+		    |
+		=====''','''
+		+---+
+		O   |
+		    |
+		    |
+		=====''','''
+		+---+
+		O   |
+		|   |
+		    |
+		=====''','''
+		+---+
+		O   |
+   	       /|   |
+		    |
+		=====''','''
+		+---+
+		O   |
+  	       /|\  |
+		    |
+		=====''','''
+		+---+
+		O   |
+	       /|\  |
+	       /    |
+		=====''','''
+		+---+
+		O   |
+	       /|\  |
+	       / \  |
+		=====''']
